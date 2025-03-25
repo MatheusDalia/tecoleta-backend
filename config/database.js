93aa6873e65f6c8f799f1d1ba8681eb527832a7d
MatheusDalia <matheusdalia8@gmail.com>
2025-03-25 12:59:10 -0300

    port 3001

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // backend/config/database.js
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("tecomat", "matheusdalia", "mononeon", {
+const sequelize = new Sequelize("tecoleta", "tecomat", "sEfkAsd387", {
   host: "localhost",
   dialect: "postgres", // Ou 'mysql' se estiver usando MySQL
 });
