db5059bdbf532b5c8e9fc133f1897f011a044519
Matheus Dalia <matheusdalia@MacBook-Pro-de-Matheus.local>
2026-02-10 11:57:49 -0300

    updated tecoleta user

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // backend/config/database.js
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("tecoleta", "tecomat", "sEfkAsd387", {
+const sequelize = new Sequelize("tecoleta", "tecoleta", "sEfkAsd387", {
   host: "localhost",
   dialect: "postgres", // Ou 'mysql' se estiver usando MySQL
 });
